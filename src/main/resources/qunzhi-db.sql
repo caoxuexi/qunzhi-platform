ca183e61005a83be9254bddcb1ba4e920a552540
CaoStudy <969718359@qq.com>
2021-10-19 14:52:28 +0800

    更新sql文件

--- src/main/resources/qunzhi-db.sql
+++ src/main/resources/qunzhi-db.sql
@@ -11,11 +11,30 @@
  Target Server Version : 80026
  File Encoding         : 65001
 
- Date: 19/10/2021 12:38:06
+ Date: 19/10/2021 14:52:00
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin_user
+-- ----------------------------
+DROP TABLE IF EXISTS `admin_user`;
+CREATE TABLE `admin_user`  (
+  `id` int NOT NULL,
+  `account` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `mobile` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `update_time` datetime(3) NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  `delete_time` datetime(3) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin_user
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for product
@@ -49,6 +68,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `nickname` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `real_name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `profile` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
@@ -65,10 +85,10 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'default', 'Qvg6b85lKYQ4Urx02ffq2w==', NULL, '752245683@qq.com', NULL, NULL, NULL, NULL, '2021-10-17 22:19:56.482', '2021-10-17 22:33:26.684', NULL);
-INSERT INTO `user` VALUES (2, 'default', 'Qvg6b85lKYQ4Urx02ffq2w==', NULL, '752245682@qq.com', NULL, NULL, NULL, NULL, '2021-10-18 15:22:17.675', '2021-10-18 15:22:17.675', NULL);
-INSERT INTO `user` VALUES (3, 'default', 'Qvg6b85lKYQ4Urx02ffq2w==', NULL, '752245680@qq.com', NULL, NULL, NULL, NULL, '2021-10-18 20:17:09.138', '2021-10-18 20:17:09.138', NULL);
-INSERT INTO `user` VALUES (4, 'default', 'Qvg6b85lKYQ4Urx02ffq2w==', NULL, '969718359@qq.com', NULL, NULL, NULL, NULL, '2021-10-18 23:04:53.523', '2021-10-18 23:04:53.523', NULL);
+INSERT INTO `user` VALUES (1, 'default', NULL, 'Qvg6b85lKYQ4Urx02ffq2w==', NULL, '752245683@qq.com', NULL, NULL, NULL, NULL, '2021-10-17 22:19:56.482', '2021-10-17 22:33:26.684', NULL);
+INSERT INTO `user` VALUES (2, 'default', NULL, 'Qvg6b85lKYQ4Urx02ffq2w==', NULL, '752245682@qq.com', NULL, NULL, NULL, NULL, '2021-10-18 15:22:17.675', '2021-10-18 15:22:17.675', NULL);
+INSERT INTO `user` VALUES (3, 'default', NULL, 'Qvg6b85lKYQ4Urx02ffq2w==', NULL, '752245680@qq.com', NULL, NULL, NULL, NULL, '2021-10-18 20:17:09.138', '2021-10-18 20:17:09.138', NULL);
+INSERT INTO `user` VALUES (4, 'default', NULL, 'Qvg6b85lKYQ4Urx02ffq2w==', NULL, '969718359@qq.com', NULL, NULL, NULL, NULL, '2021-10-18 23:04:53.523', '2021-10-18 23:04:53.523', NULL);
 
 -- ----------------------------
 -- Table structure for user_project
