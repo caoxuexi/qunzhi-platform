7f7a3bb14144ed95b27588e2beeadfbbc936b62f
CaoStudy <969718359@qq.com>
2021-10-19 14:52:56 +0800

    添加管理员相关类

---
+++ src/main/java/com/xidian/qunzhi/mapper/AdminUserMapper.java
@@ -0,0 +1,7 @@
+package com.xidian.qunzhi.mapper;
+
+import com.xidian.qunzhi.pojo.AdminUser;
+import com.xidian.qunzhi.utils.MyMapper;
+
+public interface AdminUserMapper extends MyMapper<AdminUser> {
+}
---
+++ src/main/java/com/xidian/qunzhi/pojo/AdminUser.java
@@ -0,0 +1,123 @@
+package com.xidian.qunzhi.pojo;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Table(name = "admin_user")
+public class AdminUser {
+    @Id
+    private Integer id;
+
+    private String account;
+
+    private String password;
+
+    private String mobile;
+
+    @Column(name = "create_time")
+    private Date createTime;
+
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    @Column(name = "delete_time")
+    private Date deleteTime;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return account
+     */
+    public String getAccount() {
+        return account;
+    }
+
+    /**
+     * @param account
+     */
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    /**
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return mobile
+     */
+    public String getMobile() {
+        return mobile;
+    }
+
+    /**
+     * @param mobile
+     */
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    /**
+     * @return create_time
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * @param createTime
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     * @return update_time
+     */
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     * @param updateTime
+     */
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    /**
+     * @return delete_time
+     */
+    public Date getDeleteTime() {
+        return deleteTime;
+    }
+
+    /**
+     * @param deleteTime
+     */
+    public void setDeleteTime(Date deleteTime) {
+        this.deleteTime = deleteTime;
+    }
+}
